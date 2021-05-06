0da23402311fe8abf1d3ddc8a7432d2b890cd473
LAPTOP-4E102BIC\Usuario <Ignacioruiz9797@gmail.com>
2021-05-06 23:33:31 +0200

    Toda esa verga

---
+++ New Unity Project/Assets/Script/Jump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float jumpForce;
+    private bool jump;
+    public bool puede_saltar = true; 
+    Animator animator;
+    public Rigidbody2D grounded;
+    public bool isgrounded;
+    
+
+   
+
+    void Update()
+    {
+        if (isgrounded == true)
+        { 
+       
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jump = true;
+
+            
+               
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            player.velocity = jumpForce * Vector2.up;
+            jump = false;
+            isgrounded = false; 
+           
+        }
+    }
+  
+}
