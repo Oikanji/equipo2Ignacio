88380f9d1813bc093614cd740cb574b948ec35b0
LAPTOP-4E102BIC\Usuario <Ignacioruiz9797@gmail.com>
2021-05-06 22:05:16 +0200

    Testing physics

---
+++ New Unity Project/Assets/Script/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform transformDelPlayer;
+    public float velocidadSeguimiento;
+
+    void Update()
+    {
+        Vector3 objetivo = new Vector3(transformDelPlayer.position.x, transformDelPlayer.position.y, -10);
+        transform.position = Vector3.Lerp(transform.position, objetivo, velocidadSeguimiento * Time.deltaTime);
+
+    }
+}
---
+++ New Unity Project/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //usar desde el teclado, velocidad desde unity. tener unos limites,  basado en _fisica_: efecto aceleracion pero mas complejo o en _transform_: simple pero uniforme
+
+    public float playerSpeed;
+    public static bool movimiento;
+    public GameObject panel;
+
+    void Start()
+    {
+        movimiento = true;
+    }
+
+    void Update()
+    {
+        // && se cumple ambas condiciones, || una de las dos
+
+        //izquierda
+        if (movimiento == true)
+        {
+            if ((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
+            {
+
+                transform.Translate(playerSpeed * Vector3.left);
+
+                //if (transform.position.x < -7)
+                //{
+                //    //transform.position.Set(5, transform.position.y, transform.position.z);
+                //    transform.position = new Vector3(-7, transform.position.y, transform.position.z);
+                //}
+            }
+
+            //derecha
+            if ((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.D)))
+            {
+
+
+                transform.Translate(playerSpeed * Vector3.right);
+
+                //if (transform.position.x > 7)
+                //{
+                //    //transform.position.Set(5, transform.position.y, transform.position.z);
+                //    transform.position = new Vector3(7, transform.position.y, transform.position.z);
+                //}
+            }
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            Pausa();
+        }
+    }
+
+    void Pausa()
+    {
+        movimiento = false;
+        panel.SetActive(true);
+    }
+    private void OnMouseDown()
+    {
+        panel.SetActive(false);
+        movimiento = true;
+    }
+}
