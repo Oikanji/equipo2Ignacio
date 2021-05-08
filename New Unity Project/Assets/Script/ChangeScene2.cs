02a9c83f73c9f9df61eb9b2f5e597864d6d3a5f0
lucasxnchez <83787050+lucasxnchez@users.noreply.github.com>
2021-05-08 10:52:28 +0200

    estarosa

---
+++ New Unity Project/Assets/Script/ChangeScene2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene2 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("portal"))
+        {
+            PortalScene();
+        }
+    }
+
+    public void PortalScene()
+    {
+
+        SceneManager.LoadScene("Sunset_Day");
+
+    }
+}
